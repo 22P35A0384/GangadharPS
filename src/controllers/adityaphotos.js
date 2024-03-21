528f05dacdcb4c28fa6ee2b6cbb2b8db676163d6
22P35A0384 <sai.gangadhar2022@outlook.com>
2024-03-21 23:57:01 +0530

    .jpg

--- src/controllers/adityaphotos.js
+++ src/controllers/adityaphotos.js
@@ -1,6 +1,6 @@
 const Getimg = (req,res)=>{
     const name=  req.params.id;
-    const imgname = name.toUpperCase()
+    const imgname = name.toUpperCase()+'.jpg'
     try{
         res.sendFile(imgname,{root:"./public/images"})
     }
